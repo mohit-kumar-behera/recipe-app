1b093fbfee92b43c20a658aad54cc5efe3486b9b
mohit-kumar-behera <mohitkumar.b3399@gmail.com>
2021-11-28 11:25:43 +0530

    add user profile and view profile function

--- server/recipe/home/views.py
+++ server/recipe/home/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.core.exceptions import MultipleObjectsReturned
+
+User = get_user_model()
 
 
 def home_view(request):
@@ -9,7 +13,18 @@ def home_view(request):
 
 @login_required(login_url='/')
 def profile_view(request, username):
-  return render(request, 'home/profile.html')
+  context = {'is_other_user': False, 'username': username}
+  if request.user.username != username:
+    context['is_other_user'] = True
+    try:
+      user = User.objects.get(username=username)
+    except User.DoesNotExist:
+      messages.error(request, 'Requested user\'s profile doesnot exists')
+      return redirect('home:home')
+    except MultipleObjectsReturned:
+      user = User.objects.filter(username=username).first()
+    context['other_user'] = user
+  return render(request, 'home/profile.html', context)
 
 
 def recipe_detail_view(request, slug):
