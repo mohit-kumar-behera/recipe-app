25d8a2fda14c6c02068e0aa2970b8e07b90769af
mohit-kumar-behera <mohitkumar.b3399@gmail.com>
2021-11-30 00:03:34 +0530

    build dynamic data from database

--- server/recipe/home/views.py
+++ server/recipe/home/views.py
@@ -1,3 +1,4 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render, redirect
 from django.db import transaction
 from django.contrib import messages
@@ -11,13 +12,18 @@ User = get_user_model()
 
 
 def home_view(request):
-  return render(request, 'home/home.html')
+  recipes = Recipe.objects.all()
+  context = {'recipes': recipes}
+  return render(request, 'home/home.html', context)
 
 
 @login_required(login_url='/')
 def profile_view(request, username):
   context = {'is_other_user': False, 'username': username}
-  if request.user.username != username:
+  logged_in_user  = request.user
+  context = {'user': logged_in_user}
+
+  if logged_in_user.username != username:
     context['is_other_user'] = True
     try:
       user = User.objects.get(username=username)
@@ -26,12 +32,21 @@ def profile_view(request, username):
       return redirect('home:home')
     except MultipleObjectsReturned:
       user = User.objects.filter(username=username).first()
-    context['other_user'] = user
+    context['user'] = user
+
+  recipes_of_user = context['user'].recipe_set.all()
+  context['recipes'] = recipes_of_user
   return render(request, 'home/profile.html', context)
 
 
 def recipe_detail_view(request, slug):
-  return render(request, 'home/recipe.html')
+  recipe = Recipe.objects.filter(slug=slug).first()
+  ingredients = recipe.ingredient_set.all()
+  if not recipe:
+    messages.error(request, 'Sorry, No such recipe found')
+  context = {'recipe': recipe, 'ingredients': ingredients}
+  print("here", ingredients)
+  return render(request, 'home/recipe.html', context)
 
 
 @login_required(login_url='/')
@@ -44,6 +59,10 @@ def recipe_submit_view(request):
   if request.method == 'POST':
     post_body = request.POST
     post_files = request.FILES
+
+    if not request.FILES.get('image'):
+      messages.error(request, 'Please add an Image for recipe')
+      return redirect('home:recipe_create')
 
     try:
       recipe = Recipe.objects.create(
@@ -67,3 +86,7 @@ def recipe_submit_view(request):
     return redirect('home:profile', username=request.user.username)
   messages.error(request, 'Something went very wrong')
   return redirect('home:recipe_create')
+
+
+def recipe_filter_handler(request):
+  return JsonResponse('hello', safe=False)
