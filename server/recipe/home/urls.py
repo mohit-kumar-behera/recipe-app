563a385d97f4390a3d204aa30dc70359293b0e0e
mohit-kumar-behera <mohitkumar.b3399@gmail.com>
2021-11-29 21:31:28 +0530

    add recipe create form and handle form submission

--- server/recipe/home/urls.py
+++ server/recipe/home/urls.py
@@ -6,5 +6,7 @@ app_name = 'home'
 urlpatterns = [
   path('', views.home_view, name='home'),
   path('user/<str:username>', views.profile_view, name='profile'),
-  path('recipe/<slug:slug>', views.recipe_detail_view, name='recipe_detail_view')
+  path('recipe/create', views.recipe_create_view, name='recipe_create'),
+  path('recipe/create/submit', views.recipe_submit_view, name='recipe_submit'),
+  path('recipe/<slug:slug>', views.recipe_detail_view, name='recipe_detail')
 ]
--- server/recipe/home/views.py
+++ server/recipe/home/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
+from django.db import transaction
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import MultipleObjectsReturned
+
+from home.models import Recipe, Ingredient
 
 User = get_user_model()
 
@@ -29,3 +32,38 @@ def profile_view(request, username):
 
 def recipe_detail_view(request, slug):
   return render(request, 'home/recipe.html')
+
+
+@login_required(login_url='/')
+def recipe_create_view(request):
+  return render(request, 'home/recipe-form.html')
+
+
+@login_required(login_url='/')
+def recipe_submit_view(request):
+  if request.method == 'POST':
+    post_body = request.POST
+    post_files = request.FILES
+
+    try:
+      recipe = Recipe.objects.create(
+        author=request.user,
+        title=post_body.get('title'),
+        description=post_body.get('description'),
+        image=post_files.get('image')
+      )
+    except:
+      messages.error(request, 'Something went wrong while adding your recipe')
+      return redirect('home:recipe_create')
+    else:
+      try:
+        with transaction.atomic():
+          for ingredient in post_body.getlist('ingredient'):
+            recipe.ingredient_set.create(description=ingredient)
+      except:
+        messages.error(request, 'Something went wrong while adding your recipe')
+        return redirect('home:recipe_create')
+    messages.success(request, 'Recipe was added successfully')
+    return redirect('home:profile', username=request.user.username)
+  messages.error(request, 'Something went very wrong')
+  return redirect('home:recipe_create')
